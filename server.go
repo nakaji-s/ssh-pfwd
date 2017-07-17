451974ab95e493a5e30bfc80551a4a10ce6e93d4
nakaji-s <nakajima.sota.sub@gmail.com>
2017-07-17 23:36:45 +0900

    Add server.go

---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type Server struct {
+	Config Config
+}
+
+func (s Server) Start() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.PUT("/rule", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Add rule!")
+	})
+	e.GET("/rule", func(c echo.Context) error {
+		return c.String(http.StatusOK, "GET rules!")
+	})
+	e.DELETE("/rule/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Delete rule! :"+c.Param("id"))
+	})
+	e.GET("/rule/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "GET rule! :"+c.Param("id"))
+	})
+
+	go func() {
+		e.Logger.Fatal(e.Start("127.0.0.1:8080"))
+	}()
+}
