6e4b728b70e6605116c7fd4f11f95aa566434aa7
nakaji-s <nakajima.sota.sub@gmail.com>
2017-07-24 17:29:19 +0900

    Add sample table

--- static/app.js
+++ static/app.js
@@ -1,16 +1,34 @@
 new Vue({
     el: '#app',
-    data: {
-    },
     methods: {
         gets() {
             axios.get("/rules").then((response) => {
                 console.log(response.data)
-                console.log(response.data[0].Priority)
+                //console.log(response.data[0].Priority)
+                Vue.set(this, "tableData",
+                    [{
+                        date: '2016-05-03',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles'
+                    }, {
+                        date: '2016-05-02',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles'
+                    }]
+                )
             }).catch((error) => { console.log(error); });
+        },
+        handleEdit(index, row) {
+            console.log(index, row);
+        },
+        handleDelete(index, row) {
+            console.log(index, row);
         }
     },
     data: function() {
-        return { visible: false }
+        return {
+            tableData: [],
+            visible: false
+        }
     }
 });
