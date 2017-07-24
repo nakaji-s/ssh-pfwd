6ae4fbf89a7ebe2b2b8818b88825a30ba06a2250
nakaji-s <nakajima.sota.sub@gmail.com>
2017-07-25 02:38:53 +0900

    Add binding backend and frontend

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 func main() {
-	//config := Config{}
-
-	server := Server{}
-	go func() {
-		server.Start()
-	}()
-
 	// Connection settings
 	//sshAddr := "remote_ip:22"
 	sshAddr := "localhost:22"
 	localAddr := "127.0.0.1:5000"
 	remoteAddr := "127.0.0.1:8000"
+
+	config := Config{}
+	rule := Rule{SshAddr: sshAddr, LocalAddr: localAddr, RemoteAddr: remoteAddr}
+	config.AddRule(rule)
+
+	server := Server{config}
+	go func() {
+		server.Start()
+	}()
 
 	pfwd := SSHPortForward{SshAddr: sshAddr, LocalAddr: localAddr, RemoteAddr: remoteAddr}
 	go func() {
--- static/app.js
+++ static/app.js
@@ -4,18 +4,7 @@ new Vue({
         gets() {
             axios.get("/rules").then((response) => {
                 console.log(response.data)
-                //console.log(response.data[0].Priority)
-                Vue.set(this, "tableData",
-                    [{
-                        date: '2016-05-03',
-                        name: 'Tom',
-                        address: 'No. 189, Grove St, Los Angeles'
-                    }, {
-                        date: '2016-05-02',
-                        name: 'Tom',
-                        address: 'No. 189, Grove St, Los Angeles'
-                    }]
-                )
+                Vue.set(this, "tableData", response.data)
             }).catch((error) => { console.log(error); });
         },
         handleEdit(index, row) {
@@ -30,5 +19,8 @@ new Vue({
             tableData: [],
             visible: false
         }
+    },
+    created: function() {
+        this.gets()
     }
 });
