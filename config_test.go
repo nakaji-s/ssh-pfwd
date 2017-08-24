583d5b9062135e2f6e69a7bf58d2811b4ad992cd
nakaji-s <nakajima.sota.sub@gmail.com>
2017-08-24 23:46:17 +0900

    Add mock test for gorm

--- config_test.go
+++ config_test.go
@@ -3,10 +3,16 @@ package main
 import (
 	"testing"
 
+	"reflect"
+
+	"fmt"
+
+	mocket "github.com/Selvatico/go-mocket"
+	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAddRule(t *testing.T) {
+func TestInMemoryConfig_AddRule(t *testing.T) {
 	config := InMemoryConfig{}
 
 	r1 := Rule{Priority: 3}
@@ -22,4 +28,44 @@ func TestAddRule(t *testing.T) {
 
 	assert.Equal(t, len(config.Rules), 5)
 	assert.Equal(t, config.Rules, []Rule{r5, r1, r2, r4, r3})
+}
+
+func SetupTests() *gorm.DB {
+	mocket.Catcher.Register()
+	// GORM
+	db, err := gorm.Open(mocket.DRIVER_NAME, "any_string")
+	if err != nil {
+		panic(err)
+	}
+	db.LogMode(true)
+
+	return db
+}
+
+func structToMap(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	elem := reflect.ValueOf(data).Elem()
+	size := elem.NumField()
+
+	for i := 0; i < size; i++ {
+		field := elem.Type().Field(i).Name
+		value := elem.Field(i).Interface()
+		result[field] = value
+	}
+
+	return result
+}
+
+func TestSqliteConfig_AddRule(t *testing.T) {
+	db := SetupTests()
+
+	config := SqliteConfig{Db: db}
+	config.AddRule(Rule{})
+	config.AddRule(Rule{})
+
+	ret := []map[string]interface{}{structToMap(&Rule{}), structToMap(&Rule{})}
+	mocket.Catcher.NewMock().WithReply(ret)
+	rules, _ := config.GetRules()
+
+	assert.Equal(t, 2, len(rules))
 }
